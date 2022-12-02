996c4a9ddd5ab7f27db4cc53d7e6fb9f701571e1
jiqishoubi <437753880@qq.com>
2022-12-02 11:49:27 +0800

    1

---
+++ src/routes/index.tsx
@@ -0,0 +1,16 @@
+const routes = [
+  // {
+  //   path: "/",
+  //   element: <Dashboard />,
+  //   children: [
+  //     {
+  //       path: "messages",
+  //       element: <DashboardMessages />,
+  //     },
+  //     { path: "tasks", element: <DashboardTasks /> },
+  //   ],
+  // },
+  // { path: "team", element: <AboutPage /> },
+]
+
+export default routes

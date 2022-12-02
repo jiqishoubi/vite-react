8402ebbb88a449359343fc266774384d2f2cdb21
jiqishoubi <437753880@qq.com>
2022-12-02 11:37:22 +0800

    增加prettier

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,3 +1,5 @@
+// vite3 设置了"type": "module" 后你的所有js文件默认使用ESM模块规范，不支持commonjs规范，所以必须显式的声明成xxx.cjs才能标识这个是用commonjs规范的，把你的配置都改成.cjs后缀
+
 module.exports = {
   htmlWhitespaceSensitivity: 'ignore', // 解决vue闭合标签跑到下一行的问题
   semi: false, // 是否在每行末尾添加分号
